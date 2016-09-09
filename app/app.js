65e73059703408cc312b711cd0fb4efb1a993ccf
unknown <nagachaitanya1994@gmail.com>
2016-09-09 09:43:12 +0530

    added vehicledeals

---
+++ app/app.js
@@ -0,0 +1,8 @@
+(function(){
+    'use strict'
+    angular.module("vehicleDeals",[]);
+    angular.module("vehicleDeals")
+    .config([function(){
+        console.log("I'm vehicleDeals module");
+    }])
+})();
